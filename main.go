745ba30499dabd4f175b3374af60c19c7893bb16
ZiyiTsang <1034337098@qq.com>
2022-03-31 20:20:38 +0800

    add head-Content type

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 这里是 ZZY的goblog</h1>")
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w, "I am ZZY,please contact me at:1034337098@qq.com")
 	} else {
-		fmt.Fprint(w, "wrong!")
+		fmt.Fprint(w, "wrong!!")
 	}
 }
 func main() {
