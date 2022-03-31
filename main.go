3bc48daaaa5d4e2f413c0ce0bdc4d86ffbe3ff7c
ZiyiTsang <1034337098@qq.com>
2022-03-31 11:50:56 +0800

    add "about" page

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 这里是 ZZY_goblog</h1>")
+	}else if r.URL.Path=="/about"{
+		fmt.Fprint(w, "I am ZZY,please contact me at:1034337098@qq.com")
+	}else{
+		fmt.Fprint(w, "wrong!")
+	}
 }
 func main() {
 	http.HandleFunc("/", handlerFunc)
