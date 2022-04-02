2037643f8504fe0174198adc5995fe5e169d1b89
ZiyiTsang <1034337098@qq.com>
2022-04-02 16:29:14 +0800

    try Gorilla Mux

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Go_blog
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -2,40 +2,49 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func handlerFunc_root(w http.ResponseWriter, r *http.Request) {
+func handlerfunc_Root(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 这里是 ZZY的goblog</h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "wrong!!Please try again(home)")
-	}
+	fmt.Fprint(w, "<h1>Hello, 这里是 ZZY的goblog</h1>")
 }
-func handlerFunc_articles(w http.ResponseWriter, r *http.Request) {
+func handlerfunc_Articles_Index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	if r.URL.Path == "/articles/" {
-		fmt.Fprint(w, string(r.URL.Path))
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "wrong!!Please try again(article)")
-	}
+	fmt.Fprint(w, "访问文章列表")
+
 }
-func handlerFunc_about(w http.ResponseWriter, r *http.Request) {
+func handlerfunc_Articles_Store(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	if r.URL.Path == "/about" {
-		fmt.Fprint(w, "I am Ziyi Tsang,please contact me at:1034337098@qq.com")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "404!(about)")
-	}
+	fmt.Fprint(w, "创建新的文章")
+
+}
+func handlerFunc_About(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "I am Ziyi Tsang,please contact me at:1034337098@qq.com")
+
+}
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "404")
+}
+func handlerfunc_Articles_Show(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprint(w, "文章 ID："+id)
 }
 func main() {
-	router := http.NewServeMux()
-	router.HandleFunc("/", handlerFunc_root)
-	router.HandleFunc("/about", handlerFunc_about)
-	router.HandleFunc("/articles/", handlerFunc_articles)
+	router := mux.NewRouter()
+	router.HandleFunc("/", handlerfunc_Root).Methods("Get").Name("home")
+	router.HandleFunc("/about", handlerFunc_About).Methods("Get").Name("about")
+	//router.HandleFunc("/articles", handlerfunc_Articles_Index).Methods("Get").Name("article.index")
+	//router.HandleFunc("/articles", handlerfunc_Articles_Store).Methods("Post").Name("article.store")
+
+	router.HandleFunc("/articles/{id:[0-9]+}", handlerfunc_Articles_Show).Methods("Get").Name("article.show")
+	router.HandleFunc("/articles", handlerfunc_Articles_Index).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", handlerfunc_Articles_Store).Methods("POST").Name("articles.store")
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 	http.ListenAndServe(":3000", router)
 }
