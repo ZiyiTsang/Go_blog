707cfeff21c10fb04cc590820d478d915bb6aa50
ZiyiTsang <1034337098@qq.com>
2022-04-02 17:10:27 +0800

    use middleware

--- main.go
+++ main.go
@@ -7,33 +7,33 @@ import (
 )
 
 func handlerfunc_Root(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
+
 	fmt.Fprint(w, "<h1>Hello, 这里是 ZZY的goblog</h1>")
 }
 func handlerfunc_Articles_Index(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
+
 	fmt.Fprint(w, "访问文章列表")
 
 }
 func handlerfunc_Articles_Store(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "创建新的文章")
-
 }
 func handlerFunc_About(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "I am Ziyi Tsang,please contact me at:1034337098@qq.com")
-
 }
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "404")
 }
 func handlerfunc_Articles_Show(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
 	vars := mux.Vars(r)
 	id := vars["id"]
 	fmt.Fprint(w, "文章 ID："+id)
+}
+func HTML_Middleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		h.ServeHTTP(w, r)
+	})
 }
 func main() {
 	router := mux.NewRouter()
@@ -46,5 +46,6 @@ func main() {
 	router.HandleFunc("/articles", handlerfunc_Articles_Index).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles", handlerfunc_Articles_Store).Methods("POST").Name("articles.store")
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+	router.Use(HTML_Middleware)
 	http.ListenAndServe(":3000", router)
 }
