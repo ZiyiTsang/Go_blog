80268eb0e040d5e8e383a052c7dae87d6ec19a8b
ZiyiTsang <1034337098@qq.com>
2022-04-13 16:42:20 +0800

    test on all pages

--- tests/pages_test.go
+++ tests/pages_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
+	"strconv"
 	"testing"
 )
 
@@ -12,4 +13,42 @@ func TestHomepage(t *testing.T) {
 
 	assert.NoError(t, err, "Error happen!")
 	assert.Equal(t, 200, resp.StatusCode, "should return status code->200")
+}
+func TestAllPages(t *testing.T) {
+
+	baseURL := "http://localhost:3000"
+
+	var tests = []struct {
+		method   string
+		url      string
+		expected int
+	}{
+		{"GET", "/", 200},
+		{"GET", "/about", 200},
+		{"GET", "/notfound", 404},
+		{"GET", "/articles", 200},
+		{"GET", "/articles/create", 200},
+		{"GET", "/articles/3", 200},
+		{"GET", "/articles/3/edit", 200},
+		{"POST", "/articles/3", 200},
+		{"POST", "/articles", 200},
+		{"POST", "/articles/1/delete", 404},
+	}
+
+	for _, test := range tests {
+		t.Logf("URL: %v \n", test.url)
+		var (
+			resp *http.Response
+			err  error
+		)
+		switch {
+		case test.method == "POST":
+			data := make(map[string][]string)
+			resp, err = http.PostForm(baseURL+test.url, data)
+		default:
+			resp, err = http.Get(baseURL + test.url)
+		}
+		assert.NoError(t, err, "ERR URL "+test.url)
+		assert.Equal(t, test.expected, resp.StatusCode, test.url+" should return "+strconv.Itoa(test.expected))
+	}
 }
